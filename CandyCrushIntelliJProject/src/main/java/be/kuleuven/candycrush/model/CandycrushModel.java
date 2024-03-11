fd6d532c48e8e8f7f1f6473ae2bcf6d2545e00eb
VinceDriesen <vince.driesen@student.uhasselt.be>
2024-03-11 18:42:47 +0100

    JavaFX porject toegevoegd samen met een libraries folder om de maven repos in te steken

---
+++ CandyCrushIntelliJProject/src/main/java/be/kuleuven/candycrush/model/CandycrushModel.java
@@ -0,0 +1,84 @@
+package be.kuleuven.candycrush.model;
+
+import be.kuleuven.CheckNeighboursInGrid;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Random;
+
+public class CandycrushModel {
+    private String speler;
+    private ArrayList<Integer> speelbord;
+    private int width;
+    private int height;
+
+
+    public CandycrushModel(String speler) {
+        this.speler = speler;
+        speelbord = new ArrayList<>();
+        width = 4;
+        height = 4;
+
+        for (int i = 0; i < width*height; i++){
+            Random random = new Random();
+            int randomGetal = random.nextInt(5) + 1;
+            speelbord.add(randomGetal);
+        }
+    }
+
+    public static void main(String[] args) {
+        CandycrushModel model = new CandycrushModel("arne");
+        int i = 1;
+        Iterator<Integer> iter = model.getSpeelbord().iterator();
+        while(iter.hasNext()){
+            int candy = iter.next();
+            System.out.print(candy);
+            if(i% model.getWidth()==0){
+                System.out.print("\n");
+                i = 1;
+            }
+            i++;
+        }
+        System.out.print("\n");
+
+    }
+    public String getSpeler() {
+        return speler;
+    }
+
+    public ArrayList<Integer> getSpeelbord() {
+        return speelbord;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void candyWithIndexSelected(int index){
+        //TODO: update method so it also changes direct neighbours of same type and updates score
+        if (index != -1){
+            Random random = new Random();
+            int randomGetal = random.nextInt(5) + 1;
+            speelbord.set(index,randomGetal);
+        }else{
+            System.out.println("model:candyWithIndexSelected:indexWasMinusOne");
+        }
+    }
+
+    public int getIndexFromRowColumn(int row, int column) {
+        changeNeighbours(column+row*width);
+        return column+row*width;
+    }
+
+    public void changeNeighbours(int index) {
+        Iterable<Integer> neighBoursWithSameNumber = CheckNeighboursInGrid.getSameNeighboursIds(this.speelbord, this.width, this.height, index);
+
+        for(Integer neighbourIndex : neighBoursWithSameNumber) {
+            candyWithIndexSelected(neighbourIndex);
+        }
+    }
+}
